e0de1f45c0b8cddd7e4804fc6696f145fe5539a4
Patrick Curry <patrick.curry@countingup.com>
2019-11-12 10:42:52 +0000

    fix, fmt

--- dangerfile.js
+++ dangerfile.js
@@ -1,19 +1,6 @@
-import {danger, warn} from 'danger'
+import { danger, warn } from "danger";
 
-  
 // No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
+  warn("Please include a description of your PR changes.");
 }
-
-
-// Request changes to src also include changes to tests.
-const allFiles = danger.git.modified_files.concat(danger.git.created_files)
-const hasAppChanges = allFiles.some(p => includes(p, 'src/'))
-const hasTestChanges = allFiles.some(p => includes(p, '__tests__/'))
-
-if (hasAppChanges && !hasTestChanges) {
-  warn('This PR does not include changes to tests, even though it affects app code.');
-}
-
-  
